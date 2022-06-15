1c586bdb06ee033d0bba4d27f99adbeb7d4425f8
alandau <ariellelandau360@gmail.com>
2022-06-15 13:05:12 -0400

    begin using startsWith

--- src/main/java/com/feith/jar/starter/Main.java
+++ src/main/java/com/feith/jar/starter/Main.java
@@ -4,12 +4,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 
-import javax.print.DocFlavor.URL;
+import com.google.common.base.Strings;
 
 public class Main
 {
+    /**
+     * @param args
+     * @throws IOException
+     */
     public static void main( String[] args ) throws IOException
     {
         String delimiter = args[0];
@@ -21,11 +31,51 @@ public class Main
         try ( Scanner scannerToRead = new Scanner( fileToRead );
                 FileWriter fileToWrite = new FileWriter( fileToWritePath ); )
         {
-            String oneString = scannerToRead.useDelimiter( delimiter ).next().replaceAll( "\n", " " );
-            String twoString = scannerToRead.useDelimiter( "\\Z" ).next();
+            String oneString = scannerToRead.useDelimiter( delimiter ).next();
 
-            fileToWrite.append( oneString );
-            fileToWrite.append( twoString );
+            System.out.println( oneString );
+            String[] splitString = oneString.split( "\\n" );
+
+            System.out.println( splitString );
+
+            for ( int i = 0; i < splitString.length; i++ )
+            {
+
+                if ( splitString[i].startsWith( "CASEID: " ) )
+                {
+                    String sub = splitString[i].substring( 8, splitString[i].length() );
+                    System.out.println( "field001: " + sub );
+                }
+
+                if ( splitString[i].startsWith( "VISIT DATES: " ) )
+                {
+
+                    System.out.println( "dateString:" + splitString[i] );
+                    String[] dateString = splitString[i].split( " - " );
+                    String sub1 = dateString[0].substring( 13, dateString[0].length() );
+                    String sub2 = dateString[1].substring( 0, dateString[1].length() );
+
+                    System.out.println( "date1:" + sub1 );
+                    System.out.println( "date2:" + sub2 );
+
+//                  
+                    try
+                    {
+                        Date rawDate = new SimpleDateFormat( "dd MMM yyyy" ).parse( sub1 );
+                        String pattern = "dd/MM/yyyy";
+                        SimpleDateFormat dateFormat = new SimpleDateFormat( pattern );
+                        String formattedDate = dateFormat.format( rawDate );
+
+                        System.out.println( "formatted date: " + formattedDate );
+
+                    } catch ( ParseException e )
+                    {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+            ;
         } catch ( FileNotFoundException e )
         {
             System.out.println( e );
